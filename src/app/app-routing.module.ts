0c8e40e57f65eb3161ed06db35cb323b7e1f35d3
andres4320 <sergio.andres.garzon17@gmail.com>
2024-12-02 20:25:50 -0500

    Routing to Alerta Bogota with Auth Guard

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,22 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { ChomepageComponent } from './chomepage/chomepage.component';
+import { CloginComponent } from './clogin/clogin.component';
+import { CregisterComponent } from './cregister/cregister.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: '', component: ChomepageComponent },
+  { path: 'login', component: CloginComponent },
+  { path: 'register', component: CregisterComponent },
+  {
+    path: 'user',
+    loadChildren: () => import('./user/user.module').then(m => m.UserModule)
+  },
+  {
+    path: 'admin',
+    loadChildren: () => import('./admin/admin.module').then(m => m.AdminModule)
+  },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,31 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
+import { FormsModule } from '@angular/forms';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { ChomepageComponent } from './chomepage/chomepage.component';
+import { CloginComponent } from './clogin/clogin.component';
+import { CregisterComponent } from './cregister/cregister.component';
+import { CfooterComponent } from './cfooter/cfooter.component';
+import { CheaderComponent } from './cheader/cheader.component';
+import { HttpClientModule } from '@angular/common/http';
+
+
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ChomepageComponent,
+    CloginComponent,
+    CregisterComponent,
+    CfooterComponent,
+    CheaderComponent,
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    HttpClientModule,
+    AppRoutingModule,
+    FormsModule 
   ],
   providers: [],
   bootstrap: [AppComponent]
